9ac762dc3c583eb592ad334d97b0aff1b67be5a8
kevinsmasters <gmail@kevinmasters.com>
2022-03-06 09:25:46 -0500

    project bits

--- components/projects.js
+++ components/projects.js
@@ -137,20 +137,23 @@ export default function Projects() {
         </div>
         <div className="col">
           <div className="card shadow-sm">
-            <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+            <a href="https://crossvalleyhealth.com/" target="_blank">
+          <Image
+            src="/images/cross-valley.png" // Route of the image file
+            height={120} // Desired size with correct aspect ratio
+            width={207} // Desired size with correct aspect ratio
+            alt="Cross Valley Health and Medicine"
+           />
+           </a>
             <div className="card-body">
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text">Custom Bootstrap based WordPress theme. Includes accessibility enhancements and Spanish/English language translation. </p>
               <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small className="text-muted">9 mins</small>
+                
               </div>
             </div>
           </div>
         </div>
+        { /* 
         <div className="col">
           <div className="card shadow-sm">
             <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
@@ -167,6 +170,7 @@ export default function Projects() {
             </div>
           </div>
         </div>
+        */ }
       </div>
       <style jsx>{`
         .card {

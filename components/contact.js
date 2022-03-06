61c1b695d8df2ca193d41ad448548752407c9de6
kevinsmasters <gmail@kevinmasters.com>
2022-03-06 17:31:41 -0500

    adds EmailJS form handling

--- components/contact.js
+++ components/contact.js
@@ -1,4 +1,32 @@
+import { send } from 'emailjs-com';
+import { useState } from 'react';
+import{ init } from '@emailjs/browser';
+
 export default function Contact() {
+  init("18q8DZQ5-7NPEk1KH");
+  const [toSend, setToSend] = useState({
+    inputName: '',
+    inputEmail: '',
+    textarea: ''
+  });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_ow82jex',
+      'template_0jzvbzg',
+      toSend,
+      '18q8DZQ5-7NPEk1KH'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
     return(
         <>
         <a name="contact"></a>
@@ -7,19 +35,43 @@ export default function Contact() {
             <div className="col-md-6">
                 
                 <h2 className="pb-2">Contact</h2>
-                <form>
+                <form onSubmit={onSubmit}>
                 <fieldset>
                 <div className="form-group">
                     <label htmlFor="inputName" className="form-label mt-4">Name</label>
-                    <input type="text" className="form-control" id="inputName" placeholder="Enter name" />
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      id="inputName" 
+                      placeholder="Enter name" 
+                      name="inputName"
+                      value={toSend.inputName}
+                      onChange={handleChange} 
+                    />
                   </div>
                   <div className="form-group">
-                    <label htmlFor="inputEmail1" className="form-label mt-4">Email address</label>
-                    <input type="email" className="form-control" id="inputEmail1" placeholder="Enter email" />
+                    <label htmlFor="inputEmail" className="form-label mt-4">Email address</label>
+                    <input 
+                      type="email" 
+                      className="form-control" 
+                      id="inputEmail" 
+                      name="inputEmail"
+                      placeholder="Enter email" 
+                      value={toSend.email}
+                      onChange={handleChange}
+                    />
                   </div>
                   <div className="form-group">
-                    <label htmlFor="exampleTextarea" className="form-label mt-4">Message</label>
-                    <textarea className="form-control" id="exampleTextarea" rows="3"></textarea>
+                    <label htmlFor="textarea" className="form-label mt-4">Message</label>
+                    <textarea 
+                      className="form-control" 
+                      id="textarea" 
+                      rows="3"
+                      name="textarea"
+                      value={toSend.textarea}
+                      onChange={handleChange}
+                    >
+                      </textarea>
                   </div>
                   
                   <button type="submit" className="btn btn-primary mt-4">Submit</button>

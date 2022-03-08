92ca113f9e56ad21e072d8b27b2420a2c0ebaabd
kevinsmasters <gmail@kevinmasters.com>
2022-03-07 19:25:44 -0500

    added form submit event

---
+++ components/modal.js
@@ -0,0 +1,17 @@
+export default function Modal() {
+    return(
+        <>
+        <div className="row">
+        <div className="col-md-3"></div>    
+        <div className="col-md-6">
+        <div className="alert alert-dismissible alert-info text-center">
+            {/*<button type="button" className="btn-close" data-bs-dismiss="alert"></button>*/}
+            <strong>Thank You!</strong> You sent me an email.
+        </div>
+            </div>
+        <div className="col-md-3"></div>
+        </div>
+        
+        </>
+        )
+}

b7a97b5d32408e3745c7457a7e046b9481698a53
kevinsmasters <gmail@kevinmasters.com>
2022-06-20 22:21:26 -0400

    speeiburg

--- components/footer.js
+++ components/footer.js
@@ -3,9 +3,9 @@ export default function Footer() {
         <>
     <footer id="contact" className="d-flex flex-wrap justify-content-between align-items-center py-3 my-5 border-top">
     <div className="col-md-4 d-flex align-items-center">
-      <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+      {/* <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
         
-      </a>
+      </a> */}
       <span className="text-muted">© 2022 Kevin Masters</span>
     </div>
 
--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  //reactStrictMode: true,
+  //basePath: '/apps/nextjs-cpanel'
+  useFileSystemPublicRoutes: false,
 }
+
+module.exports = nextConfig

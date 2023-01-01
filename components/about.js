bb3261fabd745e0be672ac5871a9cd132cdfa8c1
kevinsmasters <gmail@kevinmasters.com>
2023-01-01 14:34:49 -0500

    update about text and replace favicon

--- components/about.js
+++ components/about.js
@@ -1,31 +1,34 @@
 export default function About() {
-    return (
-        <>
-        <a name="mission"></a>
-        <div className="row mb-3 mt-5">
+  return (
+    <>
+      <a name="mission"></a>
+      <div className="row mb-3 mt-5">
+        <div className="col-md-2"></div>
+        <div className="col-md-8">
+          <h2>About Me</h2>
+          <p>
+            I am currently doing React development as a contractor with ICF. My
+            job has me building tools and doing UX work for the US Health and
+            Human Services Office of Inspector General. It is highly satisfying
+            to have the opportunity to practice improving my skill level In
+            React.
+          </p>
+          <p>
+            This career has been enjoyable particularly because there is a
+            seemingly limitless number of different things to learn. It's really
+            fun getting into a project and knowing that I'll come out on the
+            other side with some interesting new skill or technique. I am
+            fortunate to have worked with so many smart people. There's so much
+            to pick up and I treasure the experience. It is exceedingly
+            gratifying when I have the opportunity to share what I have learned
+            with my colleagues.
+          </p>
 
-            <div className="col-md-2"></div>
-            <div className="col-md-8">
-                
-                <h2>About Me</h2>
-                <p>Presently I am working as a contractor for ICF doing React development at the US Federal Government
-                    Health and Human Services department. Having worked most of my career with small teams, practically
-                    and literally solo in many cases - it has been hugely exciting getting to work closely with so many
-                    different and talented developers. 
-                </p>
-                <p>This career has been enjoyable particularly because there is a seemingly limitless number of 
-                    different things to learn. It's really fun getting into a project and knowing that I'll come 
-                    out on the other side with some interesting new skill or technique. I am fortunate to have 
-                    worked with so many smart people. There's so much to pick up and I treasure the experience. 
-                    It is exceedingly gratifying when I have the opportunity to share what I have learned with 
-                    my colleagues.
-                </p>
-                
-{/*<p>I love music. My kids got me into bluegrass when they were young.&nbsp; I found that I had gravitated to wanting to learn the banjo and was obsessed with getting one for a few years. I finally did and with the help of a ton of YouTube watching, taking notes, and hours of practice I managed to learn some songs and am continuing to make steady progress. Learning to play banjo has given me endless joy.</p>
+          {/*<p>I love music. My kids got me into bluegrass when they were young.&nbsp; I found that I had gravitated to wanting to learn the banjo and was obsessed with getting one for a few years. I finally did and with the help of a ton of YouTube watching, taking notes, and hours of practice I managed to learn some songs and am continuing to make steady progress. Learning to play banjo has given me endless joy.</p>
 <p>One of my ultimate favorite songs - "Can't Get There From Here" by R.E.M. has a verse that says, "If you're needing inspiration, Philomath is where I go". Philomath is a town in Georgia I've never yet been to but I appreciate the notion of finding my way through the world and learning as many new things as I can.</p> */}
-            </div>
-            <div className="col-md-2"></div>
         </div>
-        </>
-    )
+        <div className="col-md-2"></div>
+      </div>
+    </>
+  );
 }
